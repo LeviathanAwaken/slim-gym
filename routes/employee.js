14f7557f2bfa78674a207c7fd93142a84d0df682
Joshua Annis <joshannis@bak.rr.com>
2019-11-13 12:40:14 -0800

    Added members

--- routes/employee.js
+++ routes/employee.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var controller = require('../controllers').employee;
 
 router.get('/', controller.findAll);
+router.get('/', controller.findByID);
 
 module.exports = router;
--- routes/member.js
+++ routes/member.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var controller = require('../controllers').member;
 
 router.get('/', controller.findAll);
+router.get('/', controller.add);
 
 module.exports = router;
