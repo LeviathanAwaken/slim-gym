523dbcf712eaaaa5f1f0c8a8f185bcc542471102
Garrett Miller <milleregem@gmail.com>
2019-11-15 14:41:27 -0800

    lowercase exerciseset name

--- models/exerciseset.js
+++ models/exerciseset.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    var Exerciseset= sequelize.define('Exerciseset', {
+    var Exerciseset= sequelize.define('exerciseset', {
        exercisename: { type: DataTypes.STRING, allowNull: false },
        weight: { type: DataTypes.INTEGER, allowNull: false },
        reps: { type: DataTypes.INTEGER, allowNull: false},
