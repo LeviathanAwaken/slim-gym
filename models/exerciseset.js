bbf4ba79af707143352ebb19b8cffdd1229a99cf
Garrett Miller <milleregem@gmail.com>
2019-11-15 17:01:43 -0800

    change exercisename to name

--- models/exerciseset.js
+++ models/exerciseset.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
     var Exerciseset= sequelize.define('exerciseset', {
-       exercisename: { type: DataTypes.STRING, allowNull: false },
+       name: { type: DataTypes.STRING, allowNull: false },
        weight: { type: DataTypes.INTEGER, allowNull: false },
        reps: { type: DataTypes.INTEGER, allowNull: false},
        date: { type: DataTypes.INTEGER, allowNull: false }
