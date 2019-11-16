0a232e50df9e68babde7b0e7282243c1c497fee6
Joshua Annis <joshannis@bak.rr.com>
2019-11-15 18:05:30 -0800

    Modified employee and payroll

--- sql/index.js
+++ sql/index.js
@@ -1,6 +1,7 @@
 let fs = require('fs');
 let employee = fs.readFileSync(__dirname + '/mockData/employee.sql').toString();
 let payroll = fs.readFileSync(__dirname + '/mockData/payroll.sql').toString();
+//let member = fs.readFileSync(__dirname + '/mockData/member.sql').toString();
 
 let sql = employee + payroll;
 
